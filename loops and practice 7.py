54dd1cafb90b6c3ad9d2b390e6cbb9aff0d3698f
RazoR <saketsingh2410@gmail.com>
2025-11-28 00:22:17 +0530

    .

--- loops and practice 7.py
+++ loops and practice 7.py
@@ -1,32 +1,48 @@
-# 
-num = 5
-for i in range(1,11):
-    print(f"5 * {i} = {num*i}")
+# # 
+# num = 5
+# for i in range(1,11):
+#     print(f"5 * {i} = {num*i}")
 
-#
-l = ["Harry","Soham","Sachin","Rahul"]
-for i in l:
-    if i.startswith("S"):    #function to check the start word
-        print("Hello "+i)
+# #
+# l = ["Harry","Soham","Sachin","Rahul"]
+# for i in l:
+#     if i.startswith("S"):    #function to check the start word
+#         print("Hello "+i)
 
-# 
-num = 5
-i = 1
-while(i<11):
-    print(f"5 * {i} = {num*i}")
-    i = i + 1
+# # 
+# num = 5
+# i = 1
+# while(i<11):
+#     print(f"5 * {i} = {num*i}")
+#     i = i + 1
 
-#
-num = int(input("Enter a number to check if it is prime or not: "))
-count = 0
-if num <= 1:
-    print("not prime")
-else:
-    for i in range(2,num-1):
-        if num % i == 0:
-            count = 1
+# # to check prime number
+# num = int(input("Enter a number to check if it is prime or not: "))
+# count = 0
+# if num <= 1:
+#     print("not prime")
+# else:
+#     for i in range(2,num-1):
+#         if num % i == 0:
+#             count = 1
 
-    if count == 1:
-        print("not prime")
-    else:
-        print("prime")
+#     if count == 1:
+#         print("not prime")
+#     else:
+#         print("prime")
+
+# # sum of n natural numbers
+# limit = int(input("Enter the limit for sum of natural numbers: "))
+# total = 0
+# for i in range(1,limit+1):
+#     total = total+ i
+
+# print(total)
+
+# factorial
+num1 = int(input("Enter the number for factorial: "))
+fact = 1
+for i in range(1,num1+1):
+    fact = fact*i
+
+print(fact)
